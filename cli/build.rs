9cb6ed9bb6d56189a28c25804f5e924848915885
elcoosp <elcoosp@gmail.com>
2024-12-03 20:02:28 +0100

    fix: remove boilerplate by using rsmack_fs::folder_iso_struct macro instead of manual call to rsmack_utils::folder_iso_struct

--- cli/build.rs
+++ cli/build.rs
@@ -1,20 +1,12 @@
-use rsmack_utils::*;
-
+#[rsmack_fs::folder_iso_struct(from_crate = cli, folder = rule)]
+#[rsmack_wrap::wrap(with = Option)]
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+#[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
+#[serde(rename_all = "kebab-case")]
+#[serde_with::apply(Option => #[serde(skip_serializing_if = "Option::is_none")])]
+/// Rules represents the rules of commitlint.
+/// See: [rules documentation](https://commitlint.js.org/reference/rules.html)
+pub struct Rules {}
 fn main() {
-    folder_iso_struct()
-        .name("Rules")
-        .pre(quote::quote! {
-            /// Rules represents the rules of commitlint.
-            /// See: https://commitlint.js.org/reference/rules.html
-            #[rsmack_wrap::wrap(with = Option)]
-            #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
-            #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
-            #[serde(rename_all = "kebab-case")]
-            #[serde_with::apply(
-                Option => #[serde(skip_serializing_if = "Option::is_none")],
-            )]
-        })
-        .folder("rule")
-        .from_crate("cli")
-        .call();
+    Rules::generate();
 }
